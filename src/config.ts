08cf01f6a36a6d99d687d2b7cc968a0a19e07f2d
Riceball <42161586+Anderice@users.noreply.github.com>
2022-08-29 01:05:55 +0800

    feat: tailwind color setup

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,38 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
+export const SITE_BRAND = "WebDong";
+export const SITE_BRAND_SHORT = "WD";
+export const SITE_BRAND_TC = "網東";
+
 export const SITE_TITLE = "My personal website.";
 export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_SOCIAL = [
+  {
+    name: "IG",
+    url: "https://www.instagram.com/triangle_dongdong",
+  },
+  {
+    name: "YT",
+    url: "https://www.youtube.com/channel/UCIFYY0VUOND2tBdC2YDpKkQ",
+  },
+  {
+    name: "BE",
+    url: "https://www.behance.net/triangledongdong2020",
+  },
+];
 export const SITE_NAV = [
   {
     name: "Home",
-    href: "/",
+    url: "/",
   },
   {
     name: "Blog",
-    href: "/blog",
+    url: "/blog",
   },
   {
     name: "About",
-    href: "/about",
+    url: "/about",
   },
 ];
+export const SITE_COPYRIGHT = `${SITE_BRAND}&trade;  All Right Reserved`;
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,22 +3,27 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     colors: {
-      white: "#fff",
+      // Basic Setup
+      inherit: "inherit",
+      current: "currentColor",
       transparent: "transparent",
+      black: "#000",
+      white: "#fff",
       debug: "red",
+
+      // Utility Setup
       primary: {
-        50: "#f7f7f7",
-        100: "#e3e3e3",
-        200: "#c8c8c8",
-        300: "#a4a4a4",
-        400: "#818181",
-        500: "#666666",
-        600: "#515151",
-        700: "#434343",
-        800: "#383838",
-        900: "#000000",
+        50: "var(--primary-color-50)",
+        100: "var(--primary-color-100)",
+        200: "var(--primary-color-200)",
+        300: "var(--primary-color-300)",
+        400: "var(--primary-color-400)",
+        500: "var(--primary-color-500)",
+        600: "var(--primary-color-600)",
+        700: "var(--primary-color-700)",
+        800: "var(--primary-color-800)",
+        900: "var(--primary-color-900)",
       },
     },
   },
-  plugins: [],
 };
