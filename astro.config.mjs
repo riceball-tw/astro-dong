0b7b4e660bbdafe8eadda815bc65a1fcaca59a49
Riceball <42161586+Anderice@users.noreply.github.com>
2022-09-02 21:42:09 +0800

    feat: category + prettier

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,7 +12,10 @@ export default defineConfig({
     mdx(),
     sitemap(),
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
+    image(),
   ],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
   theme: {
     fontFamily: {
       mono: [
@@ -65,5 +66,46 @@ module.exports = {
         900: "var(--primary-color-900)",
       },
     },
+    extend: {
+      typography: ({ theme }) => ({
+        primary: {
+          css: {
+            "--tw-prose-body": theme("colors.primary[300]"),
+            "--tw-prose-headings": theme("colors.primary[900]"),
+            "--tw-prose-lead": theme("colors.primary[700]"),
+            "--tw-prose-links": theme("colors.primary[900]"),
+            "--tw-prose-bold": theme("colors.primary[900]"),
+            "--tw-prose-counters": theme("colors.primary[600]"),
+            "--tw-prose-bullets": theme("colors.primary[400]"),
+            "--tw-prose-hr": theme("colors.primary[300]"),
+            "--tw-prose-quotes": theme("colors.primary[900]"),
+            "--tw-prose-quote-borders": theme("colors.primary[300]"),
+            "--tw-prose-captions": theme("colors.primary[700]"),
+            "--tw-prose-code": theme("colors.primary[900]"),
+            "--tw-prose-pre-code": theme("colors.primary[100]"),
+            "--tw-prose-pre-bg": theme("colors.primary[900]"),
+            "--tw-prose-th-borders": theme("colors.primary[300]"),
+            "--tw-prose-td-borders": theme("colors.primary[200]"),
+            "--tw-prose-invert-body": theme("colors.primary[200]"),
+            "--tw-prose-invert-headings": theme("colors.white"),
+            "--tw-prose-invert-lead": theme("colors.primary[300]"),
+            "--tw-prose-invert-links": theme("colors.white"),
+            "--tw-prose-invert-bold": theme("colors.white"),
+            "--tw-prose-invert-counters": theme("colors.primary[400]"),
+            "--tw-prose-invert-bullets": theme("colors.primary[600]"),
+            "--tw-prose-invert-hr": theme("colors.primary[700]"),
+            "--tw-prose-invert-quotes": theme("colors.primary[100]"),
+            "--tw-prose-invert-quote-borders": theme("colors.primary[700]"),
+            "--tw-prose-invert-captions": theme("colors.primary[400]"),
+            "--tw-prose-invert-code": theme("colors.white"),
+            "--tw-prose-invert-pre-code": theme("colors.primary[300]"),
+            "--tw-prose-invert-pre-bg": "rgb(0 0 0 / 50%)",
+            "--tw-prose-invert-th-borders": theme("colors.primary[600]"),
+            "--tw-prose-invert-td-borders": theme("colors.primary[700]"),
+          },
+        },
+      }),
+    },
   },
+  plugins: [require("@tailwindcss/typography")],
 };
