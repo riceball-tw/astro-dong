1e7d2c0ac1c10e743ecc1cf8c667fb38f0b91c13
Riceball <42161586+Anderice@users.noreply.github.com>
2022-09-29 05:32:28 +0800

    feat: category

--- src/utils/queryFetch.js
+++ src/utils/queryFetch.js
@@ -1,10 +1,11 @@
-export async function queryFetch(query) {
+export async function queryFetch(query, queryVar) {
   const graphqlAPI = import.meta.env.STRAPI_API + "/graphql";
   const response = await fetch(graphqlAPI, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       query,
+      variables: queryVar
     }),
   });
   const data = await response.json();
