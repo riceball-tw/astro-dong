d1d1dbc9d1a33cf854d0f1e7ba1e60ac4d53d072
Riceball <42161586+Anderice@users.noreply.github.com>
2022-11-02 01:57:39 +0800

    feat: remove rss

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://www.webdong.dev",
   integrations: [
     mdx(),
     sitemap(),
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,10 +1,12 @@
-import rss from "@astrojs/rss";
-import { SITE_BRAND, SITE_DESCRIPTION } from "@config";
+// import rss from "@astrojs/rss";
+// import { SITE_BRAND, SITE_DESCRIPTION } from "@config";
+// import { queryFetch } from "@utils/queryFetch";
 
-export const get = () =>
-  rss({
-    title: SITE_BRAND,
-    description: SITE_DESCRIPTION,
-    site: import.meta.env.SITE,
-    items: import.meta.glob("./pages/post/**/*.{mdx, md}"),
-  });
+
+// export const get = () =>
+//   rss({
+//     title: SITE_BRAND,
+//     description: SITE_DESCRIPTION,
+//     site: import.meta.env.SITE,
+//     items: import.meta.glob("./pages/post/**/*.{mdx, md}"),
+//   });
