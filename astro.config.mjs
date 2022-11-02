f843f738780d491f8caeb8f409625a0caf5be684
Riceball <42161586+Anderice@users.noreply.github.com>
2022-11-02 08:51:14 +0800

    feat: GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.webdong.dev",
@@ -15,6 +13,5 @@ export default defineConfig({
         applyBaseStyles: false,
       },
     }),
-    image(),
   ],
 });
